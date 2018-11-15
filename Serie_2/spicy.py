1977f1a79a775ecbe11d69727275376fca5514bb
Arsen Hnatiuk <hnatiuka@chapel.mathematik.hu-berlin.de>
2018-11-15 18:10:11 +0100

    wahnsinn

--- Serie_2/spicy.py
+++ Serie_2/spicy.py
@@ -1,38 +1,69 @@
-class Sparse(object):
-    """
-    Diese Klasse erlaubt das Plotten von Funktionen und ihren Ableitungen (exakt und
-    approximiert).
+import numpy as np
+from scipy.sparse import dok_matrix
 
-    Attribute:
 
-        ablex_lis (list):
-            Liste aus den ersten drei (anfangend bei 0) exakten Ableitungen der
-            zu untersuchenden Funktion. Der list-Index gibt hierbei den Grad der
-            Ableitung an, wobei die Funktion selbst als nullte Ableitung aufgefasst wird.
-        p_arr (numpy.ndarray aus floats):
-            Plotpunkte, an denen die Funktionen geplottet bzw. für die Fehlerbestimmung aus-
-            gewertet werden.
-    """
-    def __init__(slef, ):
-        """
-        Initialisiert ein neues Differenzieren-Objekt. Legt aus der gegebenen Funktion und deren
-        Ableitungen eine Liste an.
+"""
+Funktion, die eine Blockmatrix erstellt
+"""
+def block(n, k):
+    A = dok_matrix((n-1, n-1))
+    for i in range(n-1):
+        for j in range(n-1):
+            if i==j:
+                A[i, j] = 2*k
+            elif i==(j+1) or i==(j-1):
+                A[i, j] = -1
+    return A 
 
-        Input:
+"""
+Fukntion, die die Matrix der groesserer Ordnung ausfuellt
+"""
+#Für die Matrix der Dimension 2
+def ausfuellen(l, block, mat2, n):
+    for i in range(n-1):
+        for j in range(n-1):
+            if i==j or i==(j+1) or i==(j-1):
+                mat2[l*(n-1)+i, l*(n-1)+j]=block[i, j]
 
-            fkt (function):
-                Bestimmt, wohin die Funktionen gezeichnet werden.
-            abl_ex (function):
-                Exakte erste Ableitung.
-            abl2_ex (function):
-                Exakte zweite Ableitung.
-            p_arr (numpy.ndarray aus floats):
-                Plotpunkte, an denen die Funktionen geplottet bzw. für die Fehlerbestimmung aus-
-                gewertet werden.
+#Für die Matrix der Dimension 3    
+def ausfuellen2(l, mat2, mat3, n):
+    for i in range((n-1)**2):
+        if i>0:
+            mat3[l*((n-1)**2)+i, l*((n-1)**2)+i-1]=mat2[i, i-1]
+        if i<((n-1)**2-1):
+            mat3[l*((n-1)**2)+i, l*((n-1)**2)+i+1]=mat2[i, i+1]
+        if i>(n-2):
+            mat3[l*((n-1)**2)+i, l*((n-1)**2)+i-n+1]=-1
+        if i<((n-1)**2-(n-1)):
+            mat3[l*((n-1)**2)+i, l*((n-1)**2)+i+n-1]=-1
 
-        Return: -
-        """
-        self.ablex_lis = [fkt, abl_ex, abl2_ex] # Liste aus exakten Ableitungen
-        self.p_arr = p_arr
+n=50
+k=2
 
-    def plotfkt_exakt(self, plotbereich, grad=0, **kwargs):
+
+
+array1 = np.zeros((n-1)**3)
+array2 = np.zeros((n-1)**3)
+for i in range((n-1)**3):
+    array1[i] = -1
+    array2[i] = k*2
+
+mat1 = block(n, k)
+ 
+#Matrix der Dimension 2
+mat2 = dok_matrix(((n-1)**2, (n-1)**2))
+mat2.setdiag(array1, n-1)
+mat2.setdiag(array1, -(n-1))
+for i in range(n-1):
+    ausfuellen(i, mat1,mat2, n)
+
+#Metrix der Dimension 3
+mat3 = dok_matrix(((n-1)**3, (n-1)**3))
+mat3.setdiag(array1, (n-1)**2)
+mat3.setdiag(array1, -(n-1)**2)
+mat3.setdiag(array2)
+for i in range(n-1):
+    ausfuellen2(i, mat2,mat3, n)
+
+#print(mat2.todense())
+#print(mat3.todense())
