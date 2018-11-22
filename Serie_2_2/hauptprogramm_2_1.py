f960d4417ffdb41bcb2b8cf2c01fd2192de0f03a
Max Huneshagen <hunesham@mathematik.hu-berlin.de>
2018-11-22 18:49:26 +0100

    Sachen

---
+++ Serie_2_2/hauptprogramm_2_1.py
@@ -0,0 +1,67 @@
+"""
+Dieses Program dient zur Veranschaulichung der Funktionalitaet der Sparse-Klasse.
+Es wird zu der durch den Benutzer eingegebenen Feinheit der Diskretisierung
+und Raumdimension eine Koeffizientenmatrix mittles der Sparse Klasse erstellt.
+Anschliessend werden die absolute und relative Anzahl der Null- und nicht-Null-Eintraege
+ausgegeben.
+"""
+import sparse
+
+def main():
+    """
+    Hauptprogramm, das die Funktionalitaet der sparse.Sparse-Klasse demonstriert.
+
+    Input: -
+
+    Return: -
+    """
+
+    # Abfrage der Parameter dis und dim:
+
+    abbr = 1
+    while abbr == 1:
+        try:
+            dis = int(input('Geben Sie eine die Feinheit der Diskretisierung ein' +
+                            ' (eine natuerliche Zahl)\n'))
+            if dis < 1:
+                print('Nicht gueltiger Wert eingegeben. Versuchen Sie erneut.\n')
+                abbr = 1
+            else:
+                abbr = 0
+        except ValueError:
+            print('Nicht gueltiger Wert eingegeben. Versuchen Sie erneut.\n')
+            abbr = 1
+    abbr = 1
+    while abbr == 1:
+        try:
+            dim = int(input('Geben Sie die Raumdimension ein (1, 2 oder 3)\n'))
+            if dim not in [1, 2, 3]:
+                print('Nicht gueltiger Wert eingegeben. Versuchen Sie erneut.\n')
+                abbr = 1
+            else:
+                abbr = 0
+        except ValueError:
+            print('Nicht gueltiger Wert eingegeben. Versuchen Sie erneut.\n')
+            abbr = 1
+
+
+    sparse_obj = sparse.Sparse(dim, dis)
+    matrix = sparse_obj.return_mat_d()
+    print('Es wurden die Dimension {} und die Diskretisierung {} gewaehlt.'.format(dim, dis))
+
+    # Gebe Matrix aus, falls diese nicht größer als 20x20 ist:
+    if matrix.get_shape()[0] <= 20:
+        print('Die Koeffizientenmatrix ist:\n', matrix.todense())
+
+    print('Die Anzahl der nicht-Null-Eintraege in der Koeffizientenmatrix ist ' +
+          '{}'.format(sparse_obj.anz_nn_abs()))
+    print('Die Anzahl der Null-Eintraege in der Koeffizientenmatrix ist ' +
+          '{}.'.format(sparse_obj.anz_n_abs()))
+    print('Die relative Anzahl der nicht-Null-Eintraege in der Koeffizientenmatrix ist '
+          '{}.'.format(sparse_obj.anz_nn_rel()))
+    print('Die relative Anzahl der Null-Eintraege in der Koeffizientenmatrix ist '
+          '{}.'.format(sparse_obj.anz_n_rel()))
+
+if __name__ == "__main__":
+    print(__doc__)
+    main()
---
+++ Serie_2_2/sparse.py
@@ -0,0 +1,204 @@
+"""
+sparse.py stellt die Klasse Sparse zur Verfuegung, mit der die Matrix A^(d) fuer d=1,2,3
+bestimmt und analysiert werden kann.
+"""
+import numpy as np
+import scipy.sparse as sp
+
+
+class Sparse(object):
+    """
+    Diese Klasse erlaubt das Erstellen der Matrizen A^(d) fuer d in [1,2,3]. Diese Matrizen werden
+    z. B. fuer die Berechnung der DGL u''(x)=-f(x) verwendet. Es handelt sich bei diesen Matrizen
+    um sehr duenn besetzte Block-Band-Matrizen, was die Verwendung von sog. sparse-Matrizen
+    in der numerischen Umsetzung nahelegt.
+
+    Attribute:
+
+        dim (int):
+            Raumdimension des zu untersuchenden Gebietes.
+        dis (numpy.ndarray aus floats):
+            Mass fuer die Diskretisierung des zu untersuchenden Gebietes.
+        matr (scipy.dok_matrix-Objekt):
+            A^(d) mit Diskretisierung dis.
+    """
+    def __init__(self, dim, dis):
+        """
+        Initialisiert ein neues Sparse-Objekt.
+
+        Input:
+
+            dim (int):
+                Raumdimension des zu untersuchenden Gebietes.
+            dis (int):
+                bestimmt die Feinheit der Diskretisierung.
+
+        Return: -
+        """
+        self.dim = dim
+        self.dis = dis
+        self.matr = self.constr_mat_l_k(dim, dim, dis)
+
+    def constr_mat_l_k(self, k, dim, dis):
+        """
+        Konstruiert die Matrix A_l(k) mit der gewuenschten Diskretisierung.
+
+        Input:
+
+            k (float):
+                Bestimmt den Wert auf der Hauptdiagonalen der untersuchten Matrix (=2*k)
+            dim (int, moegliche Werte: 1, 2, 3):
+                Raumdimension des betrachteten Gebietes.
+            dis (int):
+                Diskretisierung des Gebietes.
+
+        Return:
+            (scipy.sparse.dok_matrix-Objekt):
+                A_l(k) mit der gewuenschten Diskretisierung.
+        """
+        if dim == 1:
+
+            # Im Falle dim==1 werden die 3 mittleren Diagonalen gemaess Definition befuellt:
+
+            mat = sp.dok_matrix((dis-1, dis-1))
+            mat.setdiag(2*k)
+            mat.setdiag(-1, 1)
+            mat.setdiag(-1, -1)
+            return mat
+
+        elif dim == 2 or dim == 3:
+
+            # Anlegen der Matrix:
+
+            mat = sp.dok_matrix(((dis-1)**dim, (dis-1)**dim))
+
+            # Per for-Schleife wird auf die (n-1)^(d-1)-dimensionale Diagonale
+            # die Matrix A^(d-1) mit gleichem k gelegt (Rekursion):
+
+            for min_ind in (dis-1)**(dim-1)*np.arange(dis-1):
+                max_ind = min_ind + (dis-1)**(dim-1)
+                if dim == 2:
+
+                    # Der Fall d==2 wird gesondert behandelt, um einen rekursiven Aufruf der
+                    # Methode nur fuer d==3 ausfuehren zu muessen. Dies dient der Verbesserung
+                    # der Geschwindigkeit dieser Methode. Da setdiag nicht auf Teilmatrizen
+                    # anwendbar ist, werden numpy-arrays zur Indexierung verwendet. Die
+                    # folgenden drei Zeilen sind dabei analog zum Fall d==1, werden
+                    # hier aber auf Teilmatrizen einer groesseren Matrix angewendet.
+
+                    mat[np.arange(min_ind, max_ind), np.arange(min_ind, max_ind)] = 2*k
+                    mat[np.arange(min_ind, max_ind)[:-1], np.arange(min_ind, max_ind)[1:]] = -1
+                    mat[np.arange(min_ind, max_ind)[1:], np.arange(min_ind, max_ind)[:-1]] = -1
+                else:
+                    mat[min_ind:max_ind, min_ind:max_ind] = self.constr_mat_l_k(k, dim-1, dis)
+
+            # Zudem werden die beiden (n-1)**(l-1)-ten Nebendiagonalen mit -1 befuellt:
+
+            mat.setdiag(-1, (dis-1)**(dim-1))
+            mat.setdiag(-1, -(dis-1)**(dim-1))
+
+            return mat
+
+        # Ausgabe bei fehlerhaftem l:
+        print("Der Methode constr_A_k_l wurde ein falscher Wert fuer l " +
+              "uebergeben. Moeglich sind l=1,2,3")
+        return None
+
+    def return_mat_d(self):
+        """
+        Diese Methode gibt die Matrix A^(d) as sparse-Matrix zurueck.
+
+        Input: -
+
+        Return:
+            (scipy.sparse.dok_matrix-Objekt):
+                Die Matrix A^(d) als sparse-Matrix.
+        """
+        return self.matr
+
+    def anz_nn_abs(self):
+        """
+        Gibt die Anzahl von Eintraegen von A^(d) zurueck, die ungleich 0 sind.
+
+        Input: -
+
+        Return:
+            (int):
+                Anzahl von Nicht-Nulleintraegen von A^(d).
+        """
+
+        # Array mit Indizes aller Nichtnull-Eintraege von A^(d):
+
+        nn_arr = self.matr.nonzero()[0]
+
+        return len(nn_arr)
+
+    def anz_n_abs(self):
+        """
+        Gibt die Anzahl von Eintraegen von A^(d) zurueck, die gleich 0 sind.
+
+        Input: -
+
+        Return:
+            (int):
+                Anzahl von Nulleintraegen von A^(d).
+        """
+
+        # Array mit Indizes aller Nichtnull-Eintraege von A^(d):
+
+        nn_arr = self.matr.nonzero()[0]
+
+        # Dimension (Groesse) von A:
+
+        matr_dim = self.matr.get_shape()[0]**2
+
+        return matr_dim - len(nn_arr)
+
+    def anz_nn_rel(self):
+        """
+        Gibt die relative Anzahl von Eintraegen von A^(d) zurueck, die ungleich 0 sind.
+
+        Input: -
+
+        Return:
+            (int):
+                Relative Anzahl von Nulleintraegen von A^(d).
+        """
+
+        # Array mit Indizes aller Nichtnull-Eintraege von A^(d):
+
+        nn_arr = self.matr.nonzero()[0]
+
+        # Dimension (Groesse) von A:
+
+        matr_dim = self.matr.get_shape()[0]**2
+
+        return len(nn_arr)/matr_dim
+
+    def anz_n_rel(self):
+        """
+        Gibt die relative Anzahl von Eintraegen von A^(d) zurueck, die gleich 0 sind.
+
+        Input: -
+
+        Return:
+            (int):
+                Relative Anzahl von Nulleintraegen von A^(d).
+        """
+
+        # Array mit Indizes aller Nichtnull-Eintraege von A^(d):
+
+        nn_arr = self.matr.nonzero()[0]
+
+        # Dimension (Groesse) von A:
+
+        matr_dim = self.matr.get_shape()[0]**2
+
+        return 1 - len(nn_arr)/matr_dim
+
+
+if __name__ == "__main__":
+    TEST = Sparse(2, 5)
+    A = TEST.return_mat_d()
+    print(A.todense(), A.get_shape())
+    print(TEST.anz_n_rel(), TEST.anz_n_abs(), TEST.anz_nn_abs())
