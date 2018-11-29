e850786f21e371f762d8a8361a8ddcd199d886b8
Max Huneshagen <maxhuneshagen@gmail.com>
2018-11-29 19:39:00 +0100

    Berichtdings

--- Serie_2_2/experimente_abb.py
+++ Serie_2_2/experimente_abb.py
@@ -7,7 +7,7 @@ from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
                                AutoMinorLocator)
 
 
-matplotlib.rcParams.update({'font.size': 25})
+matplotlib.rcParams.update({'font.size': 30})
 plt.rc('text', usetex=True)
 matplotlib.rcParams['text.latex.preamble'] = [
     r'\usepackage{amsmath}',
@@ -26,6 +26,7 @@ def main():
     
     #ax_arr = ax_arr.flatten()
     #print(ax_arr[1])
+    colors = ["r","g","b"]
     for dim in [1, 2, 3]:
         if dim == 1:
             n_arr = np.unique(np.logspace(np.log10(3), 4, 50, dtype=int))
@@ -43,15 +44,21 @@ def main():
             k += 1
         #print(dim-1)    
         #print(ax)
-        ax.loglog(n_arr, anz_nn, label=r"$d={}$".format(dim))
+        ax.loglog(n_arr, anz_nn, label=r"Nicht-Null-Eintraege von $A^{{({})}}$ (rel. Anzahl)".format(dim), color=colors[dim-1])
+        ax.plot(n_arr, (n_arr-1)**(2*dim), label=r"vollbesetzte Matrix der Groesse $(n-1)^{}$".format(dim), ls="--", color=colors[dim-1])
+        ax.axhline(1, ls=":", color="k", alpha=0.6)
         #print(anz_n.min())
         #print(n_arr)
     #for j in np.arange(len(n_arr)):
-     #   n_arr[j] = float(n_arr[j])
+    #    n_arr[j] = float(n_arr[j])
     n_arr_fl = n_arr.astype(float)
-    for i in [2,-2,-3,-4,-5]:
-        ax.plot(n_arr_fl, n_arr_fl**i, label="{}".format(i))
-    ax.legend()
+   # for i in [2,-2,-3,-4,-5]:
+   #     ax.plot(n_arr_fl, n_arr_fl**i, label="{}".format(i))
+    ax.set_xlabel(r"$n$")
+    ax.set_ylabel(r"Eintraege")
+    ax.legend(loc=(0.5,0.2), framealpha=1)
+    plt.subplots_adjust(left=0.07, right=0.99, bottom=0.09, top=0.99)
+    plt.savefig("Bericht/Bilder/nn_eintraege.png", dpi=300)
     plt.show()
 
 if __name__ == "__main__":
