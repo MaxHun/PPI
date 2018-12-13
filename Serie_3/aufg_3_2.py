5471b0af21ae12f0fcddda9511c45eb70272c75f
Arsen Hnatiuk <hnatiuka@decker.mathematik.hu-berlin.de>
2018-12-13 19:16:15 +0100

    augf 2, 3 fertig

--- Serie_3/aufg_3_2.py
+++ Serie_3/aufg_3_2.py
@@ -2,10 +2,12 @@
 Dieses Modul löst die Aufgaben 3.2 und 3.3, indem es die Differentialgleichung löst und
 das Verhalten der Lösungen grafisch aufstellt.
 """
-from sparse_erw import Sparse
 from plot_disc_fct import plot_disc_fct
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import numpy as np
+from sparse_erw import Sparse
 
 def modo(m_nb, numb):
     """
@@ -19,10 +21,9 @@ def modo(m_nb, numb):
         (int):
             Ausgabe der modifizierten Modolo Methode
     """
-    if m_nb%(numb-1)==0:
-        return(numb-1)
-    else:
-        return(m_nb%(numb-1))
+    if m_nb%(numb-1) == 0:
+        return numb-1
+    return m_nb%(numb-1)
 
 def gitter(numb, dims):
     """
@@ -35,40 +36,40 @@ def gitter(numb, dims):
             Dimension der Diskretisierung
     Return:
         arra (ndarray):
-            Array mit den Punkten auf dem Diskretisierungsgitter        
+            Array mit den Punkten auf dem Diskretisierungsgitter
     """
     #Dimension 1
-    if dims==1:
+    if dims == 1:
         arra = np.zeros(numb-1)
         for i in range(numb-1):
-            arra[i] = (i+1)/n
-        return(arra)    
+            arra[i] = (i+1)/numb
+        return arra
     #Dimension 2
-    if dims==2:
+    if dims == 2:
         arra = np.zeros(((numb-1)**2, 2))
         for i in range((numb-1)**2):
             arra[i] = [modo(i+1, numb)/numb, (i//(numb-1)+1)/numb]
-        return(arra)
+        return arra
     #Dimension 3
-    if dims == 3:
-        arra = np.zeros(((numb-1)**3, 3))
-        for i in range((numb-1)**3):
-            arra[i] = [modo(i+1, numb)/numb, modo((i//(numb-1)+1), numb)/numb, (i//((numb-1)**2)+1)/numb]
-        return(arra)
-    
-def fkt(x):
+    arra = np.zeros(((numb-1)**3, 3))
+    for i in range((numb-1)**3):
+        arra[i] = [modo(i+1, numb)/numb, modo((i//(numb-1)+1), numb)/numb,
+                   (i//((numb-1)**2)+1)/numb]
+    return arra
+
+def fntn(wert):
     """
     Beispielfunktion
     Input:
-        x (float):
-            Wert, auf dem die Funktion ausgewertet wird.
+        wert (array von float):
+            Werte, auf dem die Funktion ausgewertet wird.
     Return:
         (float):
             Wert der Funktion
     """
-    return(1000*np.sin(1000*x[0])*(x[1]))
+    return 1000*np.sin(1000*wert[0])*(wert[1])
 
-def loesg(numb, dims, fkt, ulsg=fkt):
+def loesg(numb, dims, fkt, ulsg=fntn):
     """
     Diese Methode dient zur Lösung der Differentialgleichung und zum Vergleichen der exakten
     und approxmierten Lösungen.
@@ -81,7 +82,7 @@ def loesg(numb, dims, fkt, ulsg=fkt):
             Die gegebene Funktion f aus der Aufgabestellung
         ulsg (Funktion):
             Die exakte Lösung der Differentialgleichung
-            
+
     Return:
         (float):
             Der absolute Fehler in der approximierten Lösung der Differentialgleichung
@@ -91,31 +92,53 @@ def loesg(numb, dims, fkt, ulsg=fkt):
     arrb = np.zeros((numb-1)**dims)
     for i in range((numb-1)**dims):
         arrb[i] = fkt(arra[i])
+
     #Erstellung und Lösen der Bandmatrix
     mata = Sparse(dims, numb)
     lsg = mata.lgs_lsg(arrb)
+
     #Erstellung des Vektors der exakten Lösung
     arrex = np.zeros((numb-1)**dims)
     for i in range((numb-1)**dims):
         arrex[i] = ulsg(arra[i])
+
     #Erstellung des Vektors des Fehlers in der berechneten Lösung
     arrf = np.zeros((numb-1)**dims)
     for i in range((numb-1)**dims):
         arrf[i] = np.abs(arrex[i]-lsg[i])
+
     #Grafische Ausgabe
     if dims == 2:
+
+        #Plot der berechneten Lösung
         plot_disc_fct(lsg, numb, "Berechnete Lösung mit Feinheit der Diskretisierung "+str(numb))
-        
-        
-        plot_disc_fct(arrf, numb, "Fehler bezüglich der Referenzlösung mit Feinheit der Diskretisierung "+str(numb))
-        plt.show()    
-    return(np.amax(arrf))
-        
+
+        #Plot der Referenzlösung
+        grd1 = np.linspace(0, 1, 1000)
+        grd2 = np.linspace(0, 1, 1000)
+        grd1, grd2 = np.meshgrid(grd1, grd2)
+        arra2 = gitter(1001, 2)
+        arrex2 = np.zeros(1000000)
+        for i in range(1000000):
+            arrex2[i] = ulsg(arra2[i])
+        grd3 = arrex2.reshape(1000, 1000)
+        fig = plt.figure()
+        fig.suptitle("Die Referenzlösung")
+        axis = Axes3D(fig)
+        axis.plot_surface(grd1, grd2, grd3, cmap=cm.coolwarm)
+        axis.view_init(20, -105)
+
+        #Plot des Fehlers
+        plot_disc_fct(arrf, numb, "Fehler bezüglich der Referenzlösung mit"
+                      +" Feinheit der Diskretisierung "+str(numb))
+        plt.show()
+    return np.amax(arrf)
+
 def main():
     """
-    In dieser Funktion wird der 
+    In dieser Funktion wird der
     """
-    maxfl = loesg(15, 2, fkt)
+    maxfl = loesg(15, 2, fntn)
     print("Der maximale Fehler ist "+str(maxfl))
-    
+
 main()
