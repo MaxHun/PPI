24787cce32e320745a418a0532471d8fc18177c7
Arsen Hnatiuk <hnatiuka@kruge.mathematik.hu-berlin.de>
2018-12-20 20:16:39 +0100

    ready to tumble

--- Serie_3_2/aufg_3_5.py
+++ Serie_3_2/aufg_3_5.py
@@ -4,10 +4,17 @@ das Verhalten der Lösungen grafisch aufstellt.
 """
 from plot_disc_fct import plot_disc_fct
 from mpl_toolkits.mplot3d import Axes3D
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
 from sparse_erw import Sparse
+
+matplotlib.rcParams.update({'font.size': 18})
+plt.rc('text', usetex=True)
+matplotlib.rcParams['text.latex.preamble'] = [
+    r'\usepackage{amsmath}',
+    r'\usepackage{amssymb}', r"\usepackage{nicefrac}"]
 
 def modo(m_nb, numb):
     """
@@ -92,7 +99,7 @@ def fntn3(wert):
             Wert der Funktion
     """
     return 2*(wert[0]*(1-wert[0])*wert[2]*(1-wert[2])+wert[1]*(1-wert[1])*wert[0]*(1-wert[0])+
-             wert[1]*(1-wert[1])*wert[2]*(1-wert[2]))
+              wert[1]*(1-wert[1])*wert[2]*(1-wert[2]))
 
 def ulsg1(wert):
     """
@@ -130,13 +137,11 @@ def ulsg3(wert):
     """
     return wert[0]*(1-wert[0])*wert[1]*(1-wert[1])*wert[2]*(1-wert[2])
 
-def loesg(numb, dims, fkt, ulsg):
+def loesg(dims, fkt, ulsg):
     """
     Diese Methode dient zur Lösung der Differentialgleichung und zum Vergleichen der exakten
     und approxmierten Lösungen.
     Input:
-        numb (int):
-            Feinheit der Diskretisierung
         dims (int):
             Dimension der Diskretisierung
         fkt (Funktion):
@@ -148,51 +153,137 @@ def loesg(numb, dims, fkt, ulsg):
         (float):
             Der absolute Fehler in der approximierten Lösung der Differentialgleichung
     """
-    #Erstellung des Vektors b
-    arra = gitter(numb, dims)
-    arrb = np.zeros((numb-1)**dims)
-    for i in range((numb-1)**dims):
-        arrb[i] = fkt(arra[i])
+    #Plotten von dem Konvergenzverfahren, Dimension 1
+    if dims == 1:
+        arrn = np.arange(4, 804, 20)
+        arrfa = np.zeros(len(arrn))
+        refe = 0
+        for k in arrn:
+            #Erstellung des Vektors b
+            arra = gitter(k, dims)
+            arrb = np.zeros((k-1)**dims)
+            for i in range((k-1)**dims):
+                arrb[i] = fkt(arra[i])
 
-    #Erstellung und Lösen der Bandmatrix
-    mata = Sparse(dims, numb)
-    lsg = mata.lgs_lsg(arrb)
+            #Erstellung und Lösen der Bandmatrix
+            mata = Sparse(dims, k)
+            lsg = mata.lgs_lsg(arrb)
 
-    #Erstellung des Vektors der exakten Lösung
-    arrex = np.zeros((numb-1)**dims)
-    for i in range((numb-1)**dims):
-        arrex[i] = ulsg(arra[i])
+            #Erstellung des Vektors der exakten Lösung
+            arrex = np.zeros((k-1)**dims)
+            for i in range((k-1)**dims):
+                arrex[i] = ulsg(arra[i])
 
-    #Erstellung des Vektors des Fehlers in der berechneten Lösung
-    arrf = np.zeros((numb-1)**dims)
-    for i in range((numb-1)**dims):
-        arrf[i] = np.abs(arrex[i]-lsg[i])
+            #Erstellung des Vektors des Fehlers in der berechneten Lösung
+            arrf = np.zeros((k-1)**dims)
+            for i in range((k-1)**dims):
+                arrf[i] = np.abs(arrex[i]-lsg[i])
+
+            arrfa[refe] = np.amax(arrf)
+            refe = refe + 1
+        plt.plot(arrn, arrfa)
+        plt.title("Konvergenzverfahren der numerischen Loesung in Dimension 1")
+        plt.xlabel("Feinheit der Diskretisierung")
+        plt.ylabel("Absoluter Fehler")
+
+    #Plotten von dem Konvergenzverfahren, Dimension 2
+    if dims == 2:
+        arrn = np.arange(5, 105, 5)
+        arrfa = np.zeros(len(arrn))
+        refe = 0
+        for k in arrn:
+            #Erstellung des Vektors b
+            arra = gitter(k, dims)
+            arrb = np.zeros((k-1)**dims)
+            for i in range((k-1)**dims):
+                arrb[i] = fkt(arra[i])
+
+            #Erstellung und Lösen der Bandmatrix
+            mata = Sparse(dims, k)
+            lsg = mata.lgs_lsg(arrb)
+
+            #Erstellung des Vektors der exakten Lösung
+            arrex = np.zeros((k-1)**dims)
+            for i in range((k-1)**dims):
+                arrex[i] = ulsg(arra[i])
+
+            #Erstellung des Vektors des Fehlers in der berechneten Lösung
+            arrf = np.zeros((k-1)**dims)
+            for i in range((k-1)**dims):
+                arrf[i] = np.abs(arrex[i]-lsg[i])
+
+            arrfa[refe] = np.amax(arrf)
+            refe = refe + 1
+        plt.plot(arrn, arrfa)
+        plt.title("Konvergenzverfahren der numerischen Loesung in Dimension 2")
+        plt.xlabel("Feinheit der Diskretisierung")
+        plt.ylabel("Absoluter Fehler")
+
+    #Plotten von dem Konvergenzverfahren, Dimension 3
+    if dims == 3:
+        arrn = np.arange(3, 23, 2)
+        arrfa = np.zeros(len(arrn))
+        refe = 0
+        for k in arrn:
+            #Erstellung des Vektors b
+            arra = gitter(k, dims)
+            arrb = np.zeros((k-1)**dims)
+            for i in range((k-1)**dims):
+                arrb[i] = fkt(arra[i])
+
+            #Erstellung und Lösen der Bandmatrix
+            mata = Sparse(dims, k)
+            lsg = mata.lgs_lsg(arrb)
+
+            #Erstellung des Vektors der exakten Lösung
+            arrex = np.zeros((k-1)**dims)
+            for i in range((k-1)**dims):
+                arrex[i] = ulsg(arra[i])
+
+            #Erstellung des Vektors des Fehlers in der berechneten Lösung
+            arrf = np.zeros((k-1)**dims)
+            for i in range((k-1)**dims):
+                arrf[i] = np.abs(arrex[i]-lsg[i])
+
+            arrfa[refe] = np.amax(arrf)
+            refe = refe + 1
+        plt.plot(arrn, arrfa)
+        plt.title("Konvergenzverfahren der numerischen Loesung in Dimension 3")
+        plt.xlabel("Feinheit der Diskretisierung")
+        plt.ylabel("Absoluter Fehler")
+
+
 
     #Grafische Ausgabe
-    for i in [7, 11]
-        
-        #Erstellung des Vektors b
-        arra = gitter(numb, dims)
-        arrb = np.zeros((numb-1)**dims)
-        for i in range((numb-1)**dims):
-            arrb[i] = fkt(arra[i])
+    if dims == 2:
+        for k in [7, 27]:
 
-        #Erstellung und Lösen der Bandmatrix
-        mata = Sparse(dims, numb)
-        lsg = mata.lgs_lsg(arrb)
+            #Erstellung des Vektors b
+            arra = gitter(k, dims)
+            arrb = np.zeros((k-1)**dims)
+            for i in range((k-1)**dims):
+                arrb[i] = fkt(arra[i])
 
-        #Erstellung des Vektors der exakten Lösung
-        arrex = np.zeros((numb-1)**dims)
-        for i in range((numb-1)**dims):
-            arrex[i] = ulsg(arra[i])
+            #Erstellung und Lösen der Bandmatrix
+            mata = Sparse(dims, k)
+            lsg = mata.lgs_lsg(arrb)
 
-        #Erstellung des Vektors des Fehlers in der berechneten Lösung
-        arrf = np.zeros((numb-1)**dims)
-        for i in range((numb-1)**dims):
-            arrf[i] = np.abs(arrex[i]-lsg[i])
+            #Erstellung des Vektors der exakten Lösung
+            arrex = np.zeros((k-1)**dims)
+            for i in range((k-1)**dims):
+                arrex[i] = ulsg(arra[i])
 
-        #Plot der berechneten Lösung
-        plot_disc_fct(lsg, numb, "Berechnete Lösung mit Feinheit der Diskretisierung "+str(numb))
+            #Erstellung des Vektors des Fehlers in der berechneten Lösung
+            arrf = np.zeros((k-1)**dims)
+            for i in range((k-1)**dims):
+                arrf[i] = np.abs(arrex[i]-lsg[i])
+
+            #Plot der berechneten Lösung
+            plot_disc_fct(lsg, k, "Berechnete Loesung mit Feinheit der Diskretisierung "+str(k))
+
+            #Plot des Fehlers
+            plot_disc_fct(arrf, k, "Fehler bezueglich der Referenzloesung mit"
+                          +" Feinheit der Diskretisierung "+str(k))
 
         #Plot der Referenzlösung
         grd1 = np.linspace(0, 1, 1000)
@@ -204,17 +295,13 @@ def loesg(numb, dims, fkt, ulsg):
             arrex2[i] = ulsg(arra2[i])
         grd3 = arrex2.reshape(1000, 1000)
         fig = plt.figure()
-        fig.suptitle("Die Referenzlösung")
+        fig.suptitle("Die Referenzloesung")
         axis = Axes3D(fig)
         axis.plot_surface(grd1, grd2, grd3, cmap=cm.coolwarm)
         axis.view_init(20, -105)
 
-        #Plot des Fehlers
-        plot_disc_fct(arrf, numb, "Fehler bezüglich der Referenzlösung mit"
-                    +" Feinheit der Diskretisierung "+str(numb))
     plt.show()
-        
-    
+
 
     return np.amax(arrf)
 
@@ -222,11 +309,10 @@ def main():
     """
     In dieser Funktion werden alle Lösungen der Aufgabe dem Nutzer ausgegeben
     """
-    dims = 2
-    numb = 5
+    dims = 3
     fntn = [fntn1, fntn2, fntn3]
     ulsg = [ulsg1, ulsg2, ulsg3]
-    maxfl = loesg(numb, dims, fntn[dims-1], ulsg[dims-1])
+    maxfl = loesg(dims, fntn[dims-1], ulsg[dims-1])
     print("Der maximale Fehler ist "+str(maxfl))
 
 main()
