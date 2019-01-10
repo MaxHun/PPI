bdadfe028dd37f4eea1836a95abb16f058ec04d0
Arsen Hnatiuk <hnatiuka@decker.mathematik.hu-berlin.de>
2019-01-10 18:09:43 +0100

    virus.py

--- Serie_4/aufg_4_2.py
+++ Serie_4/aufg_4_2.py
@@ -1,7 +1,14 @@
 import csv
 import numpy as np
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as plt
 
-def lese(name, numb={1, 2}):
+
+
+
+
+def lese(name, numb={1, 2}, fall=1):
     """
     Diese Funktion dient zum Einlesen von Werten aus einer Textdatei
     Input:
@@ -9,6 +16,8 @@ def lese(name, numb={1, 2}):
             Pfad zur Datei, die eingelesen werden soll
         numb (int):
             Anzahl der zu berücksichtigenden Messungen
+        fall (int):
+            Kennzeichnet, ob es eine Spalte mit Einsen in die Matrix einzulegen
     Return:
         ruck(np Array):
             Array mit den zu berücksichtigenden Messungen
@@ -22,12 +31,12 @@ def lese(name, numb={1, 2}):
 
     #Erstellen daes Rückgabevektors
     laen = len(werte)
-    if 0 in numb:
+    if fall == 0:
         ruck = np.zeros((laen, len(numb)))
         for i in range(laen):
             kappa = 0
             for j in numb:
-                ruck[i][j]=int(werte[i][j])
+                ruck[i][kappa]=int(werte[i][j])
                 kappa = kappa+1
     else:
         ruck = np.zeros((laen, len(numb)+1))
@@ -39,8 +48,18 @@ def lese(name, numb={1, 2}):
             ruck[i][0] = 1
     return ruck
 
-print(lese('/u/hnatiuka/Praktikum/PPI/Serie_4/daten1.txt', {1}))
-vektb = lese('/u/hnatiuka/Praktikum/PPI/Serie_4/daten1.txt', {0})
-mat = lese('/u/hnatiuka/Praktikum/PPI/Serie_4/daten1.txt', {1})
+if __name__ == '__main__':
+    #Grafische Darstellung
+    print(lese('/u/hnatiuka/Praktikum/PPI/Serie_4/daten1.txt', {1}))
+    vektb = lese('/u/hnatiuka/Praktikum/PPI/Serie_4/daten1.txt', {0}, 0)
+    mat = lese('/u/hnatiuka/Praktikum/PPI/Serie_4/daten1.txt', {1})
+    mat0 = lese('/u/hnatiuka/Praktikum/PPI/Serie_4/daten1.txt', {1}, 0)
 
-plt.plot(
+    vektb = vektb.flatten()
+    mat0 = mat0.flatten()
+    plt.figure()
+    plt.scatter(mat0, vektb)
+    plt.xlabel('Hochwasserstand am Pegel a1')
+    plt.ylabel('Hochwasserstand am Pegel p')
+    plt.title('Hochwasserstand an verschiedenen Pegeln')
+    plt.show()
